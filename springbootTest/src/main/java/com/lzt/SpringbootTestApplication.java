7ee3524bc577a29ba4f12b2341ce3984ac7b5065
liuzhiting <13119129568@163.com>
2022-06-08 01:02:16 +0800

    9999999

---
+++ springbootTest/src/main/java/com/lzt/SpringbootTestApplication.java
@@ -0,0 +1,16 @@
+package com.lzt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestApplication
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("====");
+        SpringApplication.run(SpringbootTestApplication.class, args);
+    }
+
+}
